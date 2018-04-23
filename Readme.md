25f692b5bec32f5912e2c6824a4abe6bccdabe24
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# How to: Use Custom Elements in DXTabControl Tab Headers
+
+
+<p>In DXTabControl, you can customize content of tab item headers using the HeaderTemplate property. This template's DataContext contains an object specified in the Header property. So, you can use an empty binding to bind to the header:</p>
+
+
+```xaml
+<TextBlock Text="{Binding}"/>
+```
+
+
+<p>You can also customize the Glyph representation using the GlyphTemplate property.</p>
+
+<br/>
+
+
---
+++ VB/DXTabControlExample/MainWindow.xaml.vb
@@ -0,0 +1,32 @@
+﻿Imports DevExpress.Xpf.Core
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Namespace DXTabControlExample
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Partial Public Class MainWindow
+        Inherits DXWindow
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub BarButtonItem_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            MessageBox.Show("Hello!")
+        End Sub
+    End Class
+End Namespace
